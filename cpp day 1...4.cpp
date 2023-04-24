12315359bd2d81aeb54f8bd5c7cebec44f05e6f0
nup20 <130826435+nup20@users.noreply.github.com>
2023-04-24 18:25:43 +0530

    Add files via upload

---
+++ cpp day 1...4.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+ class  Triangle
+ {
+ public:
+    float L,B,H;
+
+
+    void setdata()
+    {
+        cout<<"Lenghth :"<<endl;
+        cin>>L;
+        cout<<"Breadth :"<<endl;
+        cin>>B;
+        cout<<"Height :"<<endl;
+        cin>>H;
+
+    }
+    void getdata()
+    {
+         cout<<"Area of Triangle:"<<L+B+H<<endl;
+         cout<<"Perimeter of Triangle:"<<(L+B+H)/2<<endl;
+
+    }
+ };
+int main()
+{
+    Triangle T;
+
+    T.setdata();
+    T.getdata();
+    return 0;
+}
