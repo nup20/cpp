3396fe264aae2c34725661127d79c63ff9358fed
nup20 <130826435+nup20@users.noreply.github.com>
2023-04-25 16:38:10 +0530

    Add files via upload

---
+++ day ..1....9.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+class Average
+{
+public:
+double num1,num2,num3;
+
+      Average()
+     {
+      cal();
+
+}
+void cal()
+{
+
+
+      cout<<"Num 1:"<<endl;
+      cin>>num1;
+      cout<<"Num 2:"<<endl;
+      cin>>num2;
+      cout<<"Num 3:"<<endl;
+      cin>>num3;
+
+       cout<<"Average of 3 Numbers are:"<<(num1+num2+num3)/2<<endl;
+
+
+}
+
+};
+int main()
+{
+     Average();
+    return 0;
+}
