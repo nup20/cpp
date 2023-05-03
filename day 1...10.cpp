7cc68b7f92f351e7bddd13b525a9a66938f968fe
nup20 <130826435+nup20@users.noreply.github.com>
2023-05-03 06:34:02 -0700

    Add files via upload

---
+++ day 1...10.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+class Volume
+{   public:
+double L=12,B=20,H=21;
+Volume()
+{
+    cout<<"Volume of Box :"<<L*B*H<<endl;
+}
+
+};
+int main()
+{
+Volume V;
+
+    return 0;
+}
