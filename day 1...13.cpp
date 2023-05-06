153d4dc57bf6a7e8672a2fa82f112b0508947aa4
nup20 <130826435+nup20@users.noreply.github.com>
2023-05-06 06:32:32 -0700

    Add files via upload

---
+++ day 1...13.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+class Employee
+{public:
+    double sal,whrs;
+void getinfo(double a,double b)
+{
+    whrs=a;
+    sal=b;
+    if(whrs<6)
+cout << " salary:" <<whrs*sal<<endl;
+}
+void Addsal(double a,double b)
+{   whrs=a;
+    sal=b;
+
+if(whrs*sal<500)
+
+    cout<<"salary:"<<(whrs*b)+818.91<<endl;
+
+}
+void Addwork(double a,double b)
+{   whrs=a;
+    sal=b;
+if(whrs>6)
+        cout<<"salary:"<<(whrs*b)+409.46<<endl;
+}
+};
+int main()
+{int a,b;
+Employee E;
+
+
+    cout << "Enter the working hours:" << endl;
+    cin>>a;
+    b=250;
+    E.getinfo(a,b);
+    E.Addsal(a,b);
+    E.Addwork(a,b);
+    return 0;
+}
