049e66cad0f2b4c04a27913af8a92d01d900caea
nup20 <130826435+nup20@users.noreply.github.com>
2023-06-14 03:03:25 -0700

    Add files via upload

---
+++ Inheritance 1.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+class Degree
+{ public:
+
+    void getDegree()
+    {
+        cout<<"I got a degree"<<endl;
+    }
+};
+class Undergraduate
+{ public:
+    void shop()
+    {
+        cout<<"I am an Undergraduate"<<endl;
+        cout<<"I am a Postgraduate" <<endl;
+    }
+};
+class Postgraduate
+{ public:
+    void shop()
+    {
+        cout<<"I am an Undergraduate"<<endl;
+        cout<<"I am a Postgraduate" <<endl;
+    }
+
+};
+int main()
+{  Degree D;
+Undergraduate U;
+Postgraduate P;
+
+D.getDegree();
+U.shop();
+P.shop();
+
+    return 0;
+}
